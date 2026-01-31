776bb338a5cebed1d668e28899bc3c53039291fc
deepakdevp <deepakdevp@gmail.com>
2026-02-01 00:01:15 +0530

    chore: remove Once UI attribution from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,17 +23,8 @@ export const Footer = () => {
         vertical="center"
       >
         <Text variant="body-default-s" onBackground="neutral-strong">
-          <Text onBackground="neutral-weak">© {currentYear} /</Text>
+          <Text onBackground="neutral-weak">© {currentYear}</Text>
           <Text paddingX="4">{person.name}</Text>
-          <Text onBackground="neutral-weak">
-            {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-            / Build your portfolio with{" "}
-            <SmartLink
-              href="https://once-ui.com/products/magic-portfolio"
-            >
-              Once UI
-            </SmartLink>
-          </Text>
         </Text>
         <Flex gap="16">
           {social.map(
